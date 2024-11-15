771d5d69c967e74140741edfe2a6ba79b7bf4665
Derenash <eyer.derenash@gmail.com>
2024-11-15 13:27:38 -0300

    Initial Commit

---
+++ grid_maps.gd
@@ -0,0 +1,38 @@
+extends Node3D
+@onready var main_grid: GridMap = $GridMap
+@onready var highlight_grid: GridMap = $HighlightMap
+var pathfinding: AStar2D = AStar2D.new()
+var top_cells: Dictionary = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var filled_cells: Array[Vector3i] = main_grid.get_used_cells()
+	for cell in filled_cells:
+		var cell_vec2i = Vector2i(cell.x, cell.z)
+		var max_height = top_cells.get(cell_vec2i)
+		if max_height == null or cell.y > max_height:
+			top_cells[cell_vec2i] = cell.y
+	
+	# Highlight tiles around coordinate (3,3) with a distance of 2
+	highlight_tiles(Vector2i(0, 0), 2)
+
+# function to highlight X tiles from x distance to a position
+func highlight_tiles(position_2d: Vector2i, distance: int) -> void:
+	var cells_to_highlight: Array[Vector2i] = []
+	for x in range(position_2d.x - distance, position_2d.x + distance + 1):
+		for z in range(position_2d.y - distance, position_2d.y + distance + 1):
+			var cell = Vector2i(x, z)
+			if cell.distance_to(position_2d) <= distance and top_cells.has(cell):
+				cells_to_highlight.append(cell)
+		
+	# Clear previous highlights
+	highlight_grid.clear()
+
+	# Set new highlights
+	for cell in cells_to_highlight:
+		var highlight_position = Vector3(cell.x, top_cells[cell] + 1, cell.y)
+		highlight_grid.set_cell_item(highlight_position, 2)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
